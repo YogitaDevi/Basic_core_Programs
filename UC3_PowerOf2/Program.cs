b8c8e112f8bb55586332d09d3c4ad7026e9e69e9
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 12:01:36 +0530

    Power_of_2

---
+++ UC3_PowerOf2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC3_PowerOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number where do you want: ");
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for (int cnt = 0; cnt <= n; cnt++) 
+            {
+                Console.WriteLine(num);
+                num *= 2;
+            }
+        }
+       
+    }
+}
