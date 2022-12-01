f98cb5ab4a2e3787facbbfb40cd3ee5f3dfdb746
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 14:21:51 +0530

    Factors

---
+++ UC5_Factors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC5_Factors
+{
+    public class factors
+    {
+       public static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Please enter the number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (b=2; a>1; b++)
+             if (a % b == 0)
+             {
+                    int x = 0;
+                    while (a % b == 0)
+                    {
+                        a /= b;
+                        x++;
+                    }
+                    Console.WriteLine($"{b} is a prime factor {x} Times");
+             }
+
+        }
+    }
+}
