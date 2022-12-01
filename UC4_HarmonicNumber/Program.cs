cac74af655f2c20c7851e448f4e7af6e7284f0b7
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 12:30:06 +0530

    HarmonicNumber

---
+++ UC4_HarmonicNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC4_HarmonicNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, n;
+            double s = 0.0;
+            Console.Write("Calculate the harmonic series.......");
+ 
+
+            Console.Write("\nInput the number of terms : ");
+           
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nHarmonic Series : ");
+            for (i=1;i<=n;i++)
+            {
+               
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\n{0}th Harmonic Values : {1} \n", n,s);
+        }
+    }
+}
