5197c98559ef0bfe56dbab596fc2c89ff6734fa9
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 14:44:40 +0530

    Compute

---
+++ UC6_Compute/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC6_Compute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Dividend is : ");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Divisor is : ");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is = " + quotient);
+            Console.WriteLine("Remainder is = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
