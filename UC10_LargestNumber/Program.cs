3a5f1d49bbe161cf6e28cf1bd9f751f84fba8e53
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 17:06:01 +0530

    LargestNumber

---
+++ UC10_LargestNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UC10_LargestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1st number is : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("2nd number is : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("3rd number is : ");
+            int num3 = int.Parse(Console.ReadLine());
+            
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("1st Number is the largest! ");
+                }
+                else
+                {
+                    Console.Write("3rd Number is the largest! ");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("2nd Number is the largest!");
+            else
+                Console.Write("3rd Number is the largest!");
+        }
+    }
+}
