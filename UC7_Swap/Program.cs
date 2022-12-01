4e714cc6542696059e87c98a687c6a029c321f55
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 14:59:58 +0530

    Swap

---
+++ UC7_Swap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC7_Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1st Number is a : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("2nd Number is b : ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a + b;       
+            b = a - b;       
+            a = a - b;    
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
